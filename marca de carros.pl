09e79f5e0bfc6c2f8390747efc7b85ddf9f7fa9c
geovane araujo <geovanearaujolopes@gmail.com>
2023-01-26 00:02:29 -0300

    first

---
+++ marca de carros.pl
@@ -0,0 +1,23 @@
+% Código de uma árvore geneologica de marcas de carros e seus carros, e
+% tendo algo em comum, é que são todas marcas fundadas no continente
+% Europeu.
+
+% Code for a genealogical tree of car brands and their cars, and having
+% something in common, is that they are all brands founded on the
+% European continent.
+
+%Universidade Federal de Uberlândia| Geovane Araújo Lopes da Silva.
+
+fundou(europa, mercedes).
+fundou(europa, vw).
+fundou(europa, audi).
+fundou(europa, bmw).
+fundou(bmw, bmwx5).
+fundou(audi, audiq3).
+fundou(vw,jettagli).
+fundou(mercedes, a45amg).
+
+suv(bmwx5).
+suv(audiq3).
+sedan(jettagli).
+hatch(gla45amg).
